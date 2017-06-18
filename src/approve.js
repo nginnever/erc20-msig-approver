adb5cfe978b94d8983bfa46938a836f26d2e478d
nginnever <ginneversource@gmail.com>
2017-06-18 23:31:13 +0200

    update to allow 0 balance

--- src/approve.js
+++ src/approve.js
@@ -6,12 +6,12 @@ const tokenAbi = require('./tokenAbi.js')
 const walletAbi = require('./walletAbi.js')
 
 // Token address 0xfe00c2ae296e752490425817339977a155e989fe
-var tokenAddr = '0x90ab6d52445a39de851d3521ea031862ca6e16e9'
+var tokenAddr = '0xfa8c5242b2d07d926a19994b4a21c35940a52837'
 var tokenContract = web3.eth.contract(tokenAbi)
 var tokenInst = tokenContract.at(tokenAddr)
 
 // Gnosis multisig wallet address 0xF8Bb210EeA6bCD92f060C58cA7d5a2b84FF03cD3
-var walletAddr = '0xF8Bb210EeA6bCD92f060C58cA7d5a2b84FF03cD3'
+var walletAddr = '0x8402468d7170b627b470a400824fd1be4089bc9d'
 var walletContract = web3.eth.contract(walletAbi)
 var walletInst = walletContract.at(walletAddr)
 
@@ -22,7 +22,7 @@ if(!web3._extend.utils.isAddress(process.argv[2])) {
 }
 
 // check supplied token amount is greater than 0
-if(process.argv[3] <= 0) {
+if(process.argv[3] < 0) {
   console.log("must approve greater than 0 tokens")
   return
 }
@@ -33,6 +33,6 @@ if(process.argv[3] <= 0) {
 // Arg2: The amount to approve
 var myCallData = tokenInst.approve.getData(process.argv[2], process.argv[3]);
 
-var tx = walletInst.submitTransaction(tokenAddr, 0, myCallData, {from:web3.eth.accounts[3], gas:500000})
+var tx = walletInst.submitTransaction(tokenAddr, 0, myCallData, {from:web3.eth.accounts[3], gas:1000000})
 
 console.log(process.argv[3] + " Tokens have been approved... tx hash: " + tx)
